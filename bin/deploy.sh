7407657d278eb078dc314996a38cd35cf82a5790
JohannesMerz <johannes_merz@gmx.de>
2021-02-08 18:11:20 +0100

    sets package to public

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,8 +5,8 @@ echo $CIRCLE_TAG
 
 if [[ $CIRCLE_TAG == *"next"* ]]; then
   echo "pre release - releasing to dist tag next"
-  yarn publish --non-interactive --dist-tag next
+  yarn publish --non-interactive --dist-tag next --access public
 else
   echo "normal release - releasing to dist tag latest"
-  yarn publish --non-interactive
+  yarn publish --non-interactive --access public
 fi
